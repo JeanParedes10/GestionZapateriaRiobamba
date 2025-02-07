6baf3172157fdf85e7e101a2290af6660ebb6f51
JeanParedes10 <mijosparedes18@gmail.com>
2025-02-06 23:15:37 -0500

    ListZapatos.cs Modificado

---
+++ GestionRiobambaZapateria/ListZapatos.cs
@@ -0,0 +1,43 @@
+﻿namespace VentasZapatos
+{
+    public class ListZapatos
+    {
+        // Lista genérica para almacenar las ventas de todos los días
+        private List<VentaDia> ventasSemana;
+
+        // Constructor de ListZapatos
+        public ListZapatos()
+        {
+            ventasSemana = new List<VentaDia>();
+        }
+
+        // Método para agregar una venta
+        public void AgregarVenta(VentaDia venta)
+        {
+            ventasSemana.Add(venta);
+        }
+
+        // Método para obtener todas las ventas
+        public List<VentaDia> ObtenerVentas()
+        {
+            return ventasSemana;
+
+        }
+
+        // Método para filtrar ventas por marca
+        public List<VentaDia> FiltrarVentasPorMarca(string marca)
+        {
+            switch (marca.ToLower())
+            {
+                case "Adidas: ":
+                    return ventasSemana.FindAll(v => v.Adidas > 0);
+                case "Nike: ":
+                    return ventasSemana.FindAll(v => v.Nike > 0);
+                case "Puma : ":
+                    return ventasSemana.FindAll(v => v.Puma > 0);
+                default:
+                    return ventasSemana;
+            }
+        }
+    }
+}
